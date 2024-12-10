073f474e100167b106487a0c216f889772fb0fce
Simone D'Amico <simodami90@hotmail.it>
2024-12-10 18:57:12 +0100

    fatti primi 2 esercizi

--- main.js
+++ main.js
@@ -1,100 +1,44 @@
-//esercizio 1
-const MIN=1;
-const MAX=6;
-let dado=Math.round(Math.random()*MAX-MIN)+MIN;
-console.log(`Il risultato del dado è ${dado}`);
-
-if (dado>3) {
-    console.log("è uscito un numero maggiore di 3");
-    
-} else {
-    console.log("è uscito un numero minore o uguale a 3");
-    
-}
-
-//esercizio 2
-let votoMax=100;
-let votoMin=1;
-let voto=Math.round(Math.random()*votoMax-votoMin)+votoMin;
-console.log(`Il voto è ${voto}`);
-
-if (voto>89) {
-    console.log('Ottimo!');    
-} else if(voto>69) {
-    console.log('Buono!');
-}else if(voto>59){
-    console.log('Insufficiente!');
-}else{
-    console.log('Insufficiente!');
-}
+// esercizio sui cicli
+// esercizio 1
+// function stampaNumeriPari(max) {
+//   let array=[];
+//   for (let i = 0; i < max; i++) {
+//     if (i%2==0) {
+//       array.push(i);
+//     }
+//   }
+//   return array;
+// }
+// let massimo =prompt("inserisci un numero massimo");
+// let arrayNumerica=stampaNumeriPari(massimo);
+// alert(arrayNumerica);
+// // esercizio 2
+// function contaCarattere(stringa,carattere) {
+//   let counter=0;
+//   let stringaArray=Array.from(stringa);
+//   for (let i = 0; i < stringaArray.length; i++) {
+//     if (stringaArray[i]==carattere) {
+//       counter++;
+//     }
+//   }
+//   alert(`Il carattere ${carattere} è presente ${counter} in ${stringa}`);
+// }
+// let ciao=prompt("Inserisci la stringa da esaminare");
+// let car=prompt("Inserisci il carattere da vedere nella stringa quante volte c'è");
+// contaCarattere(ciao, car);
 
 //esercizio 3
-let stagioneMax=12;
-let stagioneMin=1;
-let stagione=Math.round(Math.random()*stagioneMax-stagioneMin)+stagioneMin;
-console.log(stagione);
-
-switch (stagione) {
-    case 12:
-    case 1:
-    case 2:
-        console.log("Inverno");
-        break;
-    case 3:
-    case 4:
-    case 5:
-        console.log("Primavera");
-        break; 
-    case 6:
-    case 7:
-    case 8:
-        console.log("Estate");
-        break;
-    case 9:
-    case 10:
-    case 11:
-        console.log("Autunno");
-        break;
-    
-    default:
-        break;
+let somma=0;
+function sommaDispari(min,max) {
+  for (let i=min; i < max; i++) {
+    if (i%2==1) {
+      somma=somma +i;
+    }    
+  }
+  return somma;
 }
 
-//esercizio 4
-let età=Math.round(Math.random()*40);
-console.log(`L'età è ${età}`);
-
-età>=18?console.log(`Sei maggiorenne`) : console.log(`Sei minorenne`);
-
-//esercizio Bonus
-// let massimo=prompt("Inserisci un numero da cui sommare i numeri pari");
-// let sommaPari=0;
-// let i=0;
-// while(i<=massimo){
-//     if (i%2==0) {
-//         sommaPari=sommaPari+i;
-//         console.log('la somma pari è '+ sommaPari);
-        
-//     }
-//     i++;
-// }
-// console.log(sommaPari);
-
-//esercizio bonus parte 2
-let indovina=Math.round(Math.random()*9)+1;
-let j=0;
-let k=1;
-let text="Indovina il numero da 1 a 10!";
-while(j!=indovina){
-    j=Number(prompt(text));
-    if(j==indovina){
-        console.log("Congratulazioni! \n Tentativi= "+k);
-        break;
-    }else if(j<indovina){
-        text="Ritenta! Il numero da indovinare è più grande!";
-        k++;
-    }else{
-        text="Ritenta! Il numero da indovinare è più piccolo!";
-        k++;
-    }
-}
+let min=prompt("Inserisci il valore minimo dell'intevallo");
+let max=prompt("Inserisci il valore massimo dell'intevallo");
+let sommatoriaDispara=sommaDispari(min,max);
+alert(`La sommatoria dei numeri dispari nell'intervallo tra ${min} e ${max} è ${sommatoriaDispara}`);
